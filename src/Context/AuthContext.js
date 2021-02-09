848a71dbfd76b43de62d38792784bf8b13a4523b
Joshua Peavler <joshuaapeavler@gmail.com>
2021-02-09 16:52:27 -0700

    Created a login function using firebase.

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -14,6 +14,9 @@ export function AuthProvider({ children }) {
     function signup(email, password) {
         return auth.createUserWithEmailAndPassword(email, password);
     }
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password);
+    }
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
@@ -26,7 +29,8 @@ export function AuthProvider({ children }) {
 
     const value = {
         currentUser,
-        signup
+        signup,
+        login
     }
 
     return (
